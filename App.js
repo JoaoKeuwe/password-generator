92c5d8d9d14225b448b7158b327797701c8803a0
Joao keuwe <joaov2872@hotmail.com>
2024-02-20 21:57:54 -0300

    feat:

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import { Text, StyleSheet, Image, TouchableOpacity, View } from "react-native";
+import Slider from "@react-native-community/slider";
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.logo} source={require("./src/assets/logo.png")} />
+input
+      <Text styele={styles.title}>20 caracteres </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f3F3FF",
+    justifyContent: "center",
+    alignItems: "center",
   },
+  logo: {
+    marginBottom: 60
+  }
 });
+
+export default App;
